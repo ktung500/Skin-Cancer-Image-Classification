c33e930307041a694d1a27f701ed85485bf91f7f
ktung500 <66933908+ktung500@users.noreply.github.com>
2021-05-22 14:20:22 -0700

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -2,10 +2,13 @@ import pandas as pd
 import numpy as np
 from PIL import Image
 from sklearn.preprocessing import LabelEncoder
-import keras
+# import tensorflow as tf
+# from tensorflow import keras
 import streamlit as st
+from keras.models import load_model
+# import matplotlib as plt
 
-st.write("Diabetes Detection Streamlit Practice")
+st.write("Skin Disease Detection")
 
 df = pd.read_csv('Data/HAM10000_metadata.csv',
                  names=['lesion_id', 'image_id', 'dx', 'dx_type', 'age', 'sex', 'localization'])
@@ -13,7 +16,7 @@ df = pd.read_csv('Data/HAM10000_metadata.csv',
 df.drop(index=0, inplace=True)
 df = df.reset_index()
 df.head()
-
+st.dataframe(df['localization'].value_counts())
 st.dataframe(df)
 
 label_encoder = LabelEncoder()
@@ -25,12 +28,14 @@ df['dx_encodings'] = label_encoder.transform(df['dx'])
 
 df.head(5)
 
-model = keras.models.load_model('CNN_skin_lesion_model')
-
 
 def get_user_input():
-    location = st.sidebar.slider('Location', 0, 122, 72)
-    age = st.sidebar.slider('age', 0, 200)
+    age = st.sidebar.slider('Age', 0, 100)
+    sex = st.sidebar.radio('Sex', ['Male', 'Female'])
+    body = st.sidebar.selectbox("On what part of the body is the pigment in?", [
+                    'back', 'lower extremity', 'trunk', 'upper extremity', 'abdomen', 
+                    'face', 'chest', 'foot', 'hand', 'ear', 'genital', 'acral', 'unknown'])
+    
     # where = st.text_input
     uploaded_file = st.file_uploader("Choose an image...", type="jpg")
     return uploaded_file
@@ -38,47 +43,51 @@ def get_user_input():
 
 user_input = get_user_input()
 
-st.subheader('User Input: ')
-st.write(user_input)
-# st.image(user_input)
+if user_input is not None:
+    st.subheader('User Input: ')
+    st.write(user_input)
+    # st.image(user_input)
 
-x = "./Data/HAM10000_images_part_1/" + user_input.name
+    x = "./Data/HAM10000_images_part_1/" + user_input.name
 
+    def map_image(x):
+        IMAGE_SIZE = 32
+        return np.asarray(Image.open(x).resize((IMAGE_SIZE, IMAGE_SIZE)))
 
-def map_image(x):
-    IMAGE_SIZE = 32
-    return np.asarray(Image.open(x).resize((IMAGE_SIZE, IMAGE_SIZE)))
+    model = load_model('CNN_skin_lesion_model')
 
+    st.write(x)
 
-st.write(x)
+    st.write(map_image(x))
 
-st.write(map_image(x))
+    numpy_image_data = map_image(x)
 
-numpy_image_data = map_image(x)
+    st.write(numpy_image_data.shape)
 
-st.write(numpy_image_data.shape)
+    # Normalize the data first before predicting
 
-# Normalize the data first before predicting
+    numpy_image_data = numpy_image_data / 255.
 
-numpy_image_data = numpy_image_data / 255.
+    numpy_image_data = [np.array(numpy_image_data)]
 
-numpy_image_data = [np.array(numpy_image_data)]
+    y_pred = model.predict(np.array(numpy_image_data))
+    y_pred = np.squeeze(y_pred)
+    index = np.where(y_pred == y_pred.max())
 
-y_pred = model.predict(np.array(numpy_image_data))
-y_pred = np.squeeze(y_pred)
-index = np.where(y_pred == y_pred.max())
+    print(f"It is {label_encoder.inverse_transform(index[0])[0]}")
 
-print(f"It is {label_encoder.inverse_transform(index[0])[0]}")
+    dx_dictionary = {
+        'akiec': "actinic keratoses and intraepithelial carcinoma or Bowen's Disease",
+        'bcc': 'basal cell carcinoma',
+        'bkl': 'benign keratosis-like lesions',
+        'df': 'dermatofibroma',
+        'nv': 'melanocytic nevi',
+        'vasc': 'pyogenic granulomas and hemorrhage',
+        'mel': 'melanoma'}
 
-dx_dictionary = {
-    'akiec': "actinic keratoses and intraepithelial carcinoma or Bowen's Disease",
-    'bcc': 'basal cell carcinoma',
-    'bkl': 'benign keratosis-like lesions',
-    'df': 'dermatofibroma',
-    'nv': 'melanocytic nevi',
-    'vasc': 'pyogenic granulomas and hemorrhage',
-    'mel': 'melanoma'}
+    st.write(dx_dictionary[label_encoder.inverse_transform(index[0])[0]])
 
-st.write(dx_dictionary[label_encoder.inverse_transform(index[0])[0]])
+    print("You got cancer :)")
 
-print("You got cancer :)")
+else:
+    st.write("LOL")
